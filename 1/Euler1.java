6a07a8799b0aa9594cb1a9f991ac0182bcb21a5b
Zachary Kuhn <zachary.kuhn@gmail.com>
2014-09-10 07:53:41 -0400

    Remove the helper function from Java to make it equivalent to the others

--- 1/Euler1.java
+++ 1/Euler1.java
@@ -1,13 +1,9 @@
 import java.util.stream.IntStream;
 
 public class Euler1 {
-  private static boolean isMultipleOf(int x, int n) {
-    return x % n == 0;
-  }
-
-  public static void main(String args[]) {
+  public static void main(String[] args) {
     final int sum = IntStream.range(0, 1000)
-      .filter(i -> isMultipleOf(i, 3) || isMultipleOf(i, 5))
+      .filter(i -> i % 3 == 0 || i % 5 == 0)
       .sum();
 
     System.out.println(sum);
